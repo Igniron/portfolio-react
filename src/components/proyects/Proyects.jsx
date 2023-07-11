71fdb629defdc607aa88c97881b6fc7f11b546aa
Nacho <roncalnacho98@gmail.com>
2023-07-11 17:12:51 -0300

    slider update

--- src/components/proyects/Proyects.jsx
+++ src/components/proyects/Proyects.jsx
@@ -1,20 +1,35 @@
 import React from 'react'
 import { projects } from '../../assets/data/data.json'
+import Slider from 'react-slick';
 
 import './proyects.css'
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 
 export const Proyects = () => {
 
-
+  const settings = {
+    autoplay: true,
+    autoplaySpeed: 5000,
+    className: "slider",
+    dots: true,
+    infinite: true,
+    centerMode:true,
+    slidesPerRow: 1,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    speed: 500,
+  };
 
   
   return (
-  <div className="projects">
 
-    <h2 id="projects">Recent Projects</h2>
+    <div className="projects">
 
-    <div className="projects-conteiner">
+      <h2 id="projects">Recent Projects</h2>
 
+      <Slider {...settings}>
       {
         projects.map( data => (
           <React.Fragment key={ data.id }>
@@ -27,10 +42,9 @@ export const Proyects = () => {
         ))
       }
 
-
+      </Slider>
+      
     </div>
-    
-  </div>
 
   )
 }
